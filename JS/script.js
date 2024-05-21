bf5facd8fb51ca19c2313e9a4896a1313143bda9
SanjaK04 <159773919+SanjaK04@users.noreply.github.com>
2024-05-21 14:46:36 +0200

    uppdate

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,32 @@
-
+// När användaren klickar på knappen, körs denna funktion
+document.getElementById(`fetchButton`).addEventListener(`click`, function() {
+    // Anropa API för att hämta data
+    fetch(`https://randomuser.me/api/&#39`)
+    .then(function(response) {
+    // Konvertera svaret till JSON-format
+    return response.json();
+    })
+    .then(function(data) {
+    // Hämta användarinformation från data
+    var user = data.results[0];
+    // Uppdatera bild
+    document.getElementById(`userImage&`).src = user.picture.large;
+    // Uppdatera namn
+    document.getElementById(`userName`).innerText = user.name.first +``+
+    user.name.last;
+    // Uppdatera kön
+    document.getElementById(`userGender`).innerText = user.gender;
+    // Uppdatera e-post
+    document.getElementById(`userEmail`).innerText = user.email;
+    // Uppdatera telefonnummer
+    document.getElementById(`userPhone`).innerText = user.phone;
+    // Visa användarinformation
+    document.getElementById(`userInfo`).classList.remove(`hidden`);
+    })
+    .catch(function(error) {
+    // Om ett fel inträffar, skriv ut det i konsolen
+    console.error(`Error fetching data:`, error);
+    });
+    });
 
 
